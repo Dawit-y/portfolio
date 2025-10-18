b4559577d9f5cc6f2e349f57c929425904f99f5c
Dawit Yimer <dawityimer52@gmail.com>
2025-10-18 11:35:18 +0300

    fix: mobile view of about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,37 +1,37 @@
 const About = () => {
-  return (
-    <section id="about" className="py-20">
-      <h1 className="heading">
-        Little About
-        <span className="text-purple"> Me</span>
-      </h1>
-      <div className="w-1/2 mx-auto">
-        <p className="text-white mt-10">
-          Hey there! 👋 I'm Dawit Yimer, a full-stack developer with a passion
-          for building web applications that not only work flawlessly but also
-          have a sprinkle of personality. I’m the kind of developer who thinks
-          coding is like solving puzzles—except instead of missing pieces, you
-          get unexpected bugs (but hey, that's the fun part, right?).
-        </p>
-        <p className="text-white mt-2">
-          I have a love affair with Python, Django, JavaScript, React, NextJs
-          and Node.js—basically, I’m all about making things work seamlessly,
-          whether it's in the front-end or back-end (yes, I’m versatile like
-          that).
-        </p>
-        <p className="text-white mt-2">
-          When I'm not knee-deep in code, you’ll probably find me binge-watching
-          movies or chilling with friends while pretending I’m not secretly
-          coding in my head. Because who needs a break when there are ideas to
-          obsess over, right?
-        </p>
-        <p className="text-white mt-2">
-          In short: I love turning complex problems into simple, user-friendly
-          solutions and having a good laugh along the way.
-        </p>
-      </div>
-    </section>
-  );
+	return (
+		<section id="about" className="py-20 px-4 sm:px-6 lg:px-8">
+			<h1 className="heading">
+				Little About
+				<span className="text-purple"> Me</span>
+			</h1>
+			<div className="w-full sm:w-4/5 md:w-3/4 lg:w-1/2 mx-auto">
+				<p className="text-white mt-10 text-sm sm:text-base leading-relaxed">
+					Hey there! 👋 I'm Dawit Yimer, a full-stack developer with a passion
+					for building web applications that not only work flawlessly but also
+					have a sprinkle of personality. I'm the kind of developer who thinks
+					coding is like solving puzzles—except instead of missing pieces, you
+					get unexpected bugs (but hey, that's the fun part, right?).
+				</p>
+				<p className="text-white mt-4 text-sm sm:text-base leading-relaxed">
+					I have a love affair with Python, Django, JavaScript, React, NextJs
+					and Node.js—basically, I'm all about making things work seamlessly,
+					whether it's in the front-end or back-end (yes, I'm versatile like
+					that).
+				</p>
+				<p className="text-white mt-4 text-sm sm:text-base leading-relaxed">
+					When I'm not knee-deep in code, you'll probably find me binge-watching
+					movies or chilling with friends while pretending I'm not secretly
+					coding in my head. Because who needs a break when there are ideas to
+					obsess over, right?
+				</p>
+				<p className="text-white mt-4 text-sm sm:text-base leading-relaxed">
+					In short: I love turning complex problems into simple, user-friendly
+					solutions and having a good laugh along the way.
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default About;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,69 +7,70 @@ import man from "../assets/profile-pic.png";
 import cv from "../assets/cv.pdf";
 
 const Hero = () => {
-  return (
-    <div className="pb-20 pt-36">
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="white"
-        />
-        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="white" />
-        {/* <Spotlight
+	return (
+		<div className="relative min-h-screen flex flex-col justify-center items-center pb-20 pt-36">
+			<div className="absolute inset-0">
+				<Spotlight
+					className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+					fill="white"
+				/>
+				<Spotlight
+					className="h-[80vh] w-[50vw] top-10 left-full"
+					fill="white"
+				/>
+				<Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="white" />
+				{/* <Spotlight
           className="top-40 right-10 md:-right-32 md:-top-20 h-screen"
           fill="white"
         /> */}
-      </div>
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.06] bg-grid-black-100/[0.2]
-       absolute top-0 left-0 flex items-center justify-center"
-      >
-        <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+			</div>
+
+			<div
+				className="absolute inset-0 w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.06] bg-grid-black-100/[0.2]
+       flex items-center justify-center"
+			>
+				<div
+					className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div>
+				/>
+			</div>
 
-      <div className="flex flex-col md:flex-row items-center justify-center gap-2 relative my-15 z-10">
-        <div className="flex-1 max-w-full  md:max-w-[50vw]  flex items-center justify-center">
-          <img
-            src={man}
-            width={400}
-            height={400}
-            alt="profile pic"
-            className="rounded-full aspect-square object-cover"
-          />
-        </div>
-        <div className="max-w-full  md:max-w-[50vw] mt-5 md:mt-0 flex flex-1 flex-col items-center justify-center">
-          <TextGenerateEffect
-            words="Hi, I'm Dawit Yimer. I'm a Full Stack Developer expertise in Django, MERN and NextJs"
-            className="text-center text-[10px] md:text-3xl lg:text-4xl"
-          />
+			<div className="flex flex-col md:flex-row items-center justify-center gap-2 relative z-10 px-4 sm:px-6 lg:px-8">
+				<div className="flex-1 max-w-full md:max-w-[50vw] flex items-center justify-center">
+					<img
+						src={man}
+						width={400}
+						height={400}
+						alt="profile pic"
+						className="rounded-full aspect-square object-cover w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96"
+					/>
+				</div>
+				<div className="max-w-full md:max-w-[50vw] mt-5 md:mt-0 flex flex-1 flex-col items-center justify-center">
+					<TextGenerateEffect
+						words="Hi, I'm Dawit Yimer. I'm a Full Stack Developer expertise in Django, MERN and NextJs"
+						className="text-center text-sm sm:text-lg md:text-3xl lg:text-4xl px-4"
+					/>
 
-          <div className="flex flex-col md:flex-row items-center justify-center gap-3">
-            <a href={cv}>
-              <MagicButton
-                title="Download CV"
-                icon={<MdOutlineFileDownload />}
-                position="right"
-              />
-            </a>
-            <a href="#contact">
-              <MagicButton
-                title="Let's contact"
-                icon={<FaLocationArrow />}
-                position="right"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="flex flex-col sm:flex-row items-center justify-center gap-3 mt-6">
+						<a href={cv}>
+							<MagicButton
+								title="Download CV"
+								icon={<MdOutlineFileDownload />}
+								position="right"
+							/>
+						</a>
+						<a href="#contact">
+							<MagicButton
+								title="Let's contact"
+								icon={<FaLocationArrow />}
+								position="right"
+							/>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
